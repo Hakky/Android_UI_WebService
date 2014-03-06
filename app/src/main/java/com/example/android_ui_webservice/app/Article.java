86e1d4f39b83350a9a01b568e7d8709036111d40
marcB31 <marc.bernini@ingesup.com>
2014-03-06 17:57:30 +0100

    Adding of Articles overview

---
+++ app/src/main/java/com/example/android_ui_webservice/app/Article.java
@@ -0,0 +1,45 @@
+package com.example.android_ui_webservice.app;
+
+/**
+ * Created by marc on 06/03/14.
+ */
+public class Article {
+
+    private String img;
+    private String name;
+    private String description;
+
+    public String getImg() {
+        return img;
+    }
+
+    public void setImg(String img) {
+        this.img = img;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public Double getPrice() {
+        return price;
+    }
+
+    public void setPrice(Double price) {
+        this.price = price;
+    }
+
+    private Double price;
+}
---
+++ app/src/main/java/com/example/android_ui_webservice/app/ArticleAdapter.java
@@ -0,0 +1,79 @@
+package com.example.android_ui_webservice.app;
+
+import android.content.Context;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.BaseAdapter;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ * Created by marc on 06/03/14.
+ */
+public class ArticleAdapter extends BaseAdapter {
+
+    List<Article> articlesList = new ArrayList<Article>();
+    LayoutInflater inflater;
+
+    public ArticleAdapter(Context context, List<Article> articlesList) {
+        inflater = LayoutInflater.from(context);
+        this.articlesList = articlesList;
+    }
+
+    @Override
+    public int getCount() {
+        return this.articlesList.size();
+    }
+
+    @Override
+    public Object getItem(int position) {
+        return this.articlesList.get(position);
+    }
+
+    @Override
+    public long getItemId(int position) {
+        return position;
+    }
+
+    private class ViewHolder {
+        ImageView img;
+        TextView name;
+        TextView description;
+        TextView price;
+    }
+
+    @Override
+    public View getView(int position, View convertView, ViewGroup parent) {
+
+        ViewHolder holder;
+
+        if(convertView == null) {
+            holder = new ViewHolder();
+            convertView = inflater.inflate(R.layout.item_article, null);
+
+            holder.img = (ImageView)convertView.findViewById(R.id.img);
+            holder.name = (TextView)convertView.findViewById(R.id.name);
+            holder.description = (TextView)convertView.findViewById(R.id.description);
+            holder.price = (TextView)convertView.findViewById(R.id.price);
+
+            convertView.setTag(holder);
+        } else {
+            holder = (ViewHolder) convertView.getTag();
+        }
+
+        Article art = articlesList.get(position);
+        //holder.img.setImageResource(art.getImg());
+        holder.name.setText(art.getName());
+        holder.description.setText(art.getDescription());
+        holder.price.setText(art.getPrice().toString());
+
+        return convertView;
+    }
+
+}
+
+
