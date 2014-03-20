b2bae832737ae30e8c00f0f6f19cc8b3e1826815
Vincent <mondaying@hotmail.com>
2014-03-20 12:33:52 +0100

    Ajout de l'activité choix catégories.

--- app/src/main/java/com/example/android_ui_webservice/app/CategoryChoice.java
+++ app/src/main/java/com/example/android_ui_webservice/app/CategoryChoice.java
@@ -1,26 +1,39 @@
 package com.example.android_ui_webservice.app;
 
 import android.app.Activity;
-import android.content.Intent;
+import android.app.ActionBar;
+import android.app.Fragment;
 import android.os.Bundle;
+import android.view.LayoutInflater;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
+import android.view.ViewGroup;
+import android.os.Build;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
 
-public class MainActivity extends Activity {
+public class CategoryChoice extends Activity {
+
+    public String[] catItems = new String[] { "TEUB1" , "TEUB2", "TEUB3", "TEUB4", "TEUB5", "TEUB6", "TEUB7", "TEUB8", "TEUB9", "TEUB10"  };
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
-    }
+        setContentView(R.layout.activity_category_choice);
 
+         ArrayAdapter<String> CatAdapter ;
+        CatAdapter = new ArrayAdapter<String>(this, R.id.listViewCat, catItems);
+        ListView listview = (ListView ) findViewById(R.id.listViewCat);
+        listview.setAdapter(CatAdapter);
+    }
 
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
         
         // Inflate the menu; this adds items to the action bar if it is present.
-        getMenuInflater().inflate(R.menu.main, menu);
+        getMenuInflater().inflate(R.menu.category_choice, menu);
         return true;
     }
 
@@ -36,8 +49,4 @@ public class MainActivity extends Activity {
         return super.onOptionsItemSelected(item);
     }
 
-    public void showCatalog(View v){
-        Intent intent = new Intent(this, CategoryChoice.class);
-        startActivity(intent);
-    }
 }
