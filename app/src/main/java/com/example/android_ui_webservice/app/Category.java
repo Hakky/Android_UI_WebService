9322f964cc581eb55c24109d739a866946ff3244
Vincent <mondaying@hotmail.com>
2014-03-20 15:14:18 +0100

    Merge remote-tracking branch 'origin/master'
    Conflicts:
    	app/src/main/AndroidManifest.xml
    	app/src/main/res/values/strings.xml

---
+++ app/src/main/java/com/example/android_ui_webservice/app/Category.java
@@ -0,0 +1,42 @@
+package com.example.android_ui_webservice.app;
+
+
+public class Category {
+    private int id;
+    private String name;
+    private double string;
+    private String url_image;
+
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public double getString() {
+        return string;
+    }
+
+    public void setString(double string) {
+        this.string = string;
+    }
+
+    public String getUrl_image() {
+        return url_image;
+    }
+
+    public void setUrl_image(String url_image) {
+        this.url_image = url_image;
+    }
+}
